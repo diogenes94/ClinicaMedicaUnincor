03bc905c08aed036545244df0d5f9a655000f972
diogenes94 <diogenes_afr@hotmail.com>
2026-05-13 19:01:33 -0300

    Criando tela incial

---
+++ src/main/java/com/unincor/clinicamedicaunincor/ClinicaMedicaUnincor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.unincor.clinicamedicaunincor;
+
+/**
+ *
+ * @author dioge
+ */
+public class ClinicaMedicaUnincor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
